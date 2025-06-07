2b747fffc94a4a6f8121cefaae8898b31775c058
LouieVanNuffel <161451493+LouieVanNuffel@users.noreply.github.com>
2025-06-07 13:47:22 +0200

    Removed pengo's ability to still move when he's dead

--- Pengo/MoveCommand.h
+++ Pengo/MoveCommand.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "GameObjectCommand.h"
 #include "EngineTime.h"
-#include "Subject.h"
+#include "HealthComponent.h"
 #include "Event.h"
 
 namespace dae
@@ -12,16 +12,23 @@ namespace dae
 		MoveCommand(GameObject* gameObject, float directionX, float directionY, float speed)
 			:GameObjectCommand(gameObject), m_DirectionX{ directionX }, m_DirectionY{ directionY }, m_Speed{ speed }
 		{
+			m_pHealthComponent = gameObject->GetComponent<HealthComponent>();
 		}
 
 		virtual void Execute() override
 		{
-			glm::vec3 currentPosition{ GetCharacterObject()->GetTransform().GetPosition()};
+			if (m_pHealthComponent != nullptr)
+			{
+				if (m_pHealthComponent->GetLives() <= 0) return;
+			}
+
+			glm::vec3 currentPosition{ GetCharacterObject()->GetTransform().GetPosition() };
 			GetCharacterObject()->SetPosition(currentPosition.x + m_DirectionX * m_Speed * Time::GetInstance().GetDeltaTime(),
 				currentPosition.y + m_DirectionY * m_Speed * Time::GetInstance().GetDeltaTime());
 		}
 
 	private:
+		HealthComponent* m_pHealthComponent{ nullptr };
 		float m_DirectionX;
 		float m_DirectionY;
 		float m_Speed;
